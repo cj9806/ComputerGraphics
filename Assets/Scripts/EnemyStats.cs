3589709a032649ca6213ca98b2b40d6cd56f06f8
s209019 <s209019@SEA-C10445>
2021-10-20 17:32:27 -0700

    started working on enemy navigation, tweaked attacks for enemy

--- Assets/Scripts/EnemyStats.cs
+++ Assets/Scripts/EnemyStats.cs
@@ -1,31 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyStats : MonoBehaviour
 {
     public GameObject player;
     public int health;
     [SerializeField] Animator animator;
+    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] KinematicPlayerMotor motor;
+    public Transform goal;
+    private float ranFloat = 0;
+    private float counter= 0;
+    NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
         
     }
 
     // Update is called once per frame
+    void FixedUpdate()
+    {
+        agent.destination = goal.position;
+    }
     void Update()
     {
         if(health <= 0)
         {
             Destroy(this.gameObject);
         }
-        transform.LookAt(player.transform.position);
+        Vector3 lookAt = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        transform.LookAt(lookAt);
         float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
 
         if(distanceToPlayer <= 2.1)
         {
-            animator.SetBool("Attacking", true);
+            if(ranFloat == 0)
+            {
+                ranFloat = Random.Range(1.0f, 3.0f);
+            }
+            else
+            {
+                counter += Time.deltaTime;
+                if(counter > ranFloat)
+                {
+                    animator.SetBool("Attacking", true);
+                    ranFloat = 0;
+                    counter = 0;
+                }
+            }
         }
+        animator.SetFloat("Speed", rigidbody.velocity.magnitude);
+        animator.SetBool("Grounded", motor.Grounded);
+
     }
 }
